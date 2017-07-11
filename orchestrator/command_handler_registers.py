700c2848d5341ab267e69326bac2487657450d22
Koldunov Dmitry <volgoweb@bk.ru>
2017-07-11 09:18:36 +0300

    Начал переделку на общение между компонентами через json

--- orchestrator/command_handler_registers.py
+++ orchestrator/command_handler_registers.py
@@ -1,3 +1,5 @@
+import json
+import jsonschema
 from django.utils.module_loading import import_string
 
 
@@ -31,3 +33,70 @@ def execute_command(command):
     # @TODO Вообще-то команда не должна возвращать результат. Но пока я отложил этот вопрос.
     result = handler.handle(command)
     return result
+
+
+def execute_json_command(msg_json: str):
+    msg = MessageParser().parse(msg_json)
+    command = msg['body']
+    handler_cls = command_handler_register.get(command)
+    handler = handler_cls()
+    # @TODO Вообще-то команда не должна возвращать результат. Но пока я отложил этот вопрос.
+    result = handler.handle(command)
+    return result
+
+
+MSG_SCHEMA = {
+    '$schema': 'message',
+    'type': 'object',
+    'properties': {
+        'message_type': {
+            'type': 'string',
+            'enum': ['command', 'query'],
+        },
+        'created': {
+            'type': 'string',
+            'pattern': '^\d{4}-[01]\d-[01]\d\s[012]\d:\d{2}:\d{2}$',
+        },
+        'body': {
+            'type': 'object',
+        },
+    },
+    'required': [
+        'message_type',
+        'created',
+        'body',
+    ],
+}
+
+
+COMMAND_BODY_SCHEMA_EXAMPLE = {
+    '$schema': 'command_add_to_cart',
+    'type': 'object',
+    'properties': {
+        'user_id': {
+            'type': 'number',
+        },
+        'product_id': {
+            'type': 'number',
+        },
+    },
+    'required': [
+        'user_id',
+        'product_id',
+    ],
+}
+
+
+class MessageParser(object):
+    def __init__(self):
+        self.msg_schema = MSG_SCHEMA
+
+    def parse(self, msg_json):
+        msg = json.loads(msg_json)
+        jsonschema.validate(msg, self.msg_schema)
+        msg_body_schema = self.get_msg_body_schema(msg['type'])
+        jsonschema.validate(msg['body'], msg_body_schema)
+        return msg
+
+    def get_msg_body_schema(self, msg_type):
+        return COMMAND_BODY_SCHEMA_EXAMPLE
